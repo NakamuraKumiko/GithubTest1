88899bc9051f33c35a3dd878744694bc8c933381
KUMIKON <KUMIKON@DESKTOP-698RSN1>
2022-05-28 21:40:12 +0900

    init:

---
+++ src/main/java/com/sample/sb1/MyBootAppGitTest1Application.java
@@ -0,0 +1,13 @@
+package com.sample.sb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBootAppGitTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBootAppGitTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/sb1/MyBootAppGitTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.sb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBootAppGitTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
