bfa6dc0c88b97499ea9add9d14dad49a59b72f91
KUMIKON <KUMIKON@DESKTOP-698RSN1>
2022-06-05 19:57:40 +0900

    jquery

--- src/main/java/com/sample/sb1/HeloController2.java
+++ src/main/java/com/sample/sb1/HeloController2.java
@@ -1,12 +1,11 @@
 package com.sample.sb1;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+//@Controller
 public class HeloController2 {
 
 	@RequestMapping(value="/", method=RequestMethod.GET)
